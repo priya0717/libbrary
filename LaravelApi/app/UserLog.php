a532e3c36b7cc877caa6cc066195f50e46a2320a
ShanmugaPriya <priya.sh0717@gmail.com>
2021-02-20 10:08:07 +0530

    user logs renamed

--- LaravelApi/app/UserLog.php
+++ LaravelApi/app/UserLog.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class userLogs extends Model
+class UserLog extends Model
 {
     //
     protected $fillable = ['message','ip_address','user_id'];
