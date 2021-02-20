ca39d384fa8b729378c352d5564dbc1c57467fdb
ShanmugaPriya <priya.sh0717@gmail.com>
2021-02-20 10:15:00 +0530

    formatting applied

--- LaravelApi/database/seeds/DatabaseSeeder.php
+++ LaravelApi/database/seeds/DatabaseSeeder.php
@@ -11,9 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
         $this->call(PermissionSeeder::class);
-        $this->call(RoleSeeder::class);       
+        $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
     }
 }
