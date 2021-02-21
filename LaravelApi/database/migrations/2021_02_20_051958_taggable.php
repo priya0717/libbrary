168bd647b70f7d432275aaf5537799ce0355182c
ShanmugaPriya <priya.sh0717@gmail.com>
2021-02-21 14:35:44 +0530

    taggables added

---
+++ LaravelApi/database/migrations/2021_02_20_051958_taggable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Taggable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->foreignId('taggable_id');
+            $table->string('taggable_type');
+            $table->json('meta')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taggables');//
+    }
+}
