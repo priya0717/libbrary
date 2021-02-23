23aa3f3ae60f19f8d0513125b072a81f46d016a9
ShanmugaPriya <priya.sh0717@gmail.com>
2021-02-23 18:06:04 +0530

    Routes added and formatted

--- LaravelApi/routes/api.php
+++ LaravelApi/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,26 +16,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 use App\User;
+use Orion\Facades\Orion;
 
-Route::post('register','Auth\RegisterController@registerUser');
-Route::post('login','Auth\LoginController@loginUser');
-Route::post('logout','Auth\LogoutController@logoutUser');
+Route::post('register', 'Auth\RegisterController@registerUser');
+Route::post('login', 'Auth\LoginController@loginUser');
+Route::post('logout', 'Auth\LogoutController@logoutUser');
 
 // Route::middleware('auth:sanctum')->get('/', function (Request $request) {
 //     return response()->json(['data' => 'I am logged in']);
 // });
 
-Route::group(['middleware' => ['auth:sanctum','role:admin|user']], function () {
+Route::group(['middleware' => ['auth:sanctum', 'role:admin|user']], function () {
     // Route::get('/userlist',function(){
     //     return response()->json(['data' => 'I am logged in admin',
     //                              'role' => Auth::user()->getRoleNames()
     //                             ]);
     // });
     Route::resource('profile', 'Admin\DashboardController');
+
+    Orion::resource('books', 'Api\BookController');
+    Orion::resource('authors', 'Api\AuthorController');
 });
-    
+
 // Route::post('users', function (Request $request) {
 //     //return response()->json(['requestUsers' => $request->all()]);
 //     return response()->json(['users' => User::all(), 'status' => 200]);
 // });
-
